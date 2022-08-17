eb0ca103ca13c41293155ab5014a0a974547a3e1
Savannah Jackson <boisterouscoder@gmail.com>
2022-08-17 01:08:20 -0500

    A bunch of refactoring
    Still need to make those tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,111 @@
-#![allow(non_snake_case)]
+use crdts::glist::{GList};
+use crdts::CmRDT;
+use std::error::Error;
+use std::cmp::Ordering;
+use chrono::{DateTime, FixedOffset};
+use serde::{Deserialize, Serialize};
 
 mod save;
-mod db;
+
+const FILE_EXT:&str = ".rstore";
+const DATE_FMT:&str = "%Y %b %d %H:%M:%S%.3f %z";
+
+fn fmt_file_name(name:&str, group:&str) -> String{
+    format!("{}@{}.{}", name, group, FILE_EXT)
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct LogState {
+    pub msgs:GList<MessageData>,
+	pub group:String,
+	pub name:String,
+	pub id_keys: KeyPair
+}
+impl LogState {
+    pub fn new(name:&str, group:&str, password:&str) -> LogState{
+        let filename = fmt_file_name(name, group);
+        if save::is_store_existing(&filename){
+            match save::read_store(&filename, password) {
+                Ok(state) => return state,
+                Err(e) => println!("Failed to read {}. Prog is creating a new state. The error is as follows:\n{}", filename, e)
+            };
+        }
+        LogState{
+            msgs: GList::<MessageData>::new(),
+            group:group.to_string(),
+            name:name.to_string(),
+            id_keys: KeyPair{
+                public:"".to_string(),//TODO: Change this once RSA is added
+                private:"".to_string(),//TODO: Change this once RSA is added
+            }
+        }
+    }
+    pub fn get_filename(&self) -> String {
+	 	fmt_file_name(&self.name, &self.group)
+	}
+	pub fn save(&self, password:&str) -> Result<(), Box<dyn Error>>{
+        save::write_store(self, password)?;
+        Ok(())
+	}
+
+	pub fn add_message(&mut self, msg:MessageData){
+        self.msgs.apply(self.msgs.insert_after(self.msgs.last(), msg))
+	}
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct KeyPair {
+    pub public:String,
+	pub private:String
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct MessageData {
+    pub from:String,
+    pub tag:String,
+    pub encrypted_content:String,
+    pub signature:String,
+    pub signed_time_stamp:String,
+    pub content:Option<String>
+}
+
+impl MessageData {
+    fn get_time_stamp(&self) -> DateTime<FixedOffset>{
+        //TODO: Remove Server Signature here
+        match DateTime::parse_from_str(&self.signed_time_stamp, DATE_FMT) {
+            Ok(x) => return x,
+            Err(e) => {
+                panic!("Unable to parse date {}. \n The err reads as follows:\n {}", self.signed_time_stamp, e);
+            },
+        }
+    }
+}
+
+impl PartialEq for MessageData {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_time_stamp() == other.get_time_stamp() && self.from == other.from
+    }
+}
+
+impl Eq for MessageData {}
+
+impl PartialOrd for MessageData {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let time_order = self.get_time_stamp().cmp(&other.get_time_stamp());
+        return if Ordering::Equal == time_order {
+            Some(self.from.cmp(&other.from))
+        }else{ Some(time_order) }
+    }
+}
+
+impl Ord for MessageData {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let time_order = self.get_time_stamp().cmp(&other.get_time_stamp());
+        return if Ordering::Equal == time_order {
+            self.from.cmp(&other.from)
+        }else{ time_order }
+    }
+}
 
 #[cfg(test)]
 mod tests;
--- src/save.rs
+++ src/save.rs
@@ -4,27 +4,27 @@ use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
-use crate::db::DB;
+use crate::LogState;
 
 
-pub fn isStoreExisting(filename:&str) -> bool{
+pub fn is_store_existing(filename:&str) -> bool{
  	return fs::metadata(filename).is_ok();
 }
-pub fn writeStore(db:&DB, password:&str) -> Result<(), Box<dyn Error>>{
- 	let mut file = File::create(db.getFileName())?;
+pub fn write_store(state:&LogState, password:&str) -> Result<(), Box<dyn Error>>{
+ 	let mut file = File::create(state.get_filename())?;
  	let key = new_magic_crypt!(password, 256);
- 	let data = key.encrypt_str_to_base64(serde_json::to_string(db)?);
+ 	let data = key.encrypt_str_to_base64(serde_json::to_string(state)?);
 
  	file.write_all(data.as_bytes())?;
- 	file.sync_all();
+ 	file.sync_all()?;
  	Ok(())
 }
-pub fn readStore(filename:&str, password:&str, fileExt:&str) -> Result<DB, Box<dyn Error>>{
+pub fn read_store(filename:&str, password:&str) -> Result<LogState, Box<dyn Error>>{
  	let mut file = File::open(filename)?;
  	let mut data = String::new();
- 	file.read_to_string(&mut data);
+ 	file.read_to_string(&mut data)?;
  	let key = new_magic_crypt!(password, 256);
- 	let decryptedData = key.decrypt_base64_to_string(&data)?;
+ 	let decrypted_data = key.decrypt_base64_to_string(&data)?;
 
- 	return Ok(serde_json::from_str(&decryptedData)?);
+ 	return Ok(serde_json::from_str(&decrypted_data)?);
 }
