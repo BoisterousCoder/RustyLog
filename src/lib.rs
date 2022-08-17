ea1ab8e21d22592db37aa9d1cf292bf864b45985
Savannah Jackson <boisterouscoder@gmail.com>
2022-08-17 16:31:18 -0500

    Some Tests and Some Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,49 @@
-use crdts::glist::{GList};
+use crdts::glist::{GList, Op};
+use crdts::identifier::Identifier;
 use crdts::CmRDT;
-use std::error::Error;
-use std::cmp::Ordering;
+
+use magic_crypt::MagicCryptTrait;
+use magic_crypt::new_magic_crypt;
+
 use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
 
-mod save;
+use std::fs;
+use std::fs::File;
+use std::error::Error;
+use std::cmp::Ordering;
+use std::iter::Iterator;
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use std::io::prelude::*;
 
+
+const SAVE_DIR:&str = ".";
 const FILE_EXT:&str = ".rstore";
 const DATE_FMT:&str = "%Y %b %d %H:%M:%S%.3f %z";
 
 fn fmt_file_name(name:&str, group:&str) -> String{
-    format!("{}@{}.{}", name, group, FILE_EXT)
+    format!("{}/{}@{}.{}", SAVE_DIR, calc_hash(name), calc_hash(group), FILE_EXT)
+}
+
+fn calc_hash(text:&str) -> String{
+    let mut hasher = DefaultHasher::new();
+    text.to_string().hash(&mut hasher);
+    format!("{}", hasher.finish())
+}
+
+fn read_store(filename:&str, password:&str) -> Result<LogState, Box<dyn Error>>{
+ 	let mut file = File::open(filename)?;
+ 	let mut data = String::new();
+ 	file.read_to_string(&mut data)?;
+ 	let key = new_magic_crypt!(password, 256);
+ 	let decrypted_data = key.decrypt_base64_to_string(&data)?;
+
+ 	return Ok(serde_json::from_str(&decrypted_data)?);
+}
+
+pub fn is_store_existing(filename:&str) -> bool{
+ 	return fs::metadata(filename).is_ok();
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
@@ -24,8 +56,8 @@ pub struct LogState {
 impl LogState {
     pub fn new(name:&str, group:&str, password:&str) -> LogState{
         let filename = fmt_file_name(name, group);
-        if save::is_store_existing(&filename){
-            match save::read_store(&filename, password) {
+        if is_store_existing(&filename){
+            match read_store(&filename, password) {
                 Ok(state) => return state,
                 Err(e) => println!("Failed to read {}. Prog is creating a new state. The error is as follows:\n{}", filename, e)
             };
@@ -44,12 +76,31 @@ impl LogState {
 	 	fmt_file_name(&self.name, &self.group)
 	}
 	pub fn save(&self, password:&str) -> Result<(), Box<dyn Error>>{
-        save::write_store(self, password)?;
+        let mut file = File::create(self.get_filename())?;
+     	let key = new_magic_crypt!(password, 256);
+     	let data = key.encrypt_str_to_base64(serde_json::to_string(self)?);
+
+     	file.write_all(data.as_bytes())?;
+     	file.sync_all()?;
         Ok(())
 	}
 
-	pub fn add_message(&mut self, msg:MessageData){
-        self.msgs.apply(self.msgs.insert_after(self.msgs.last(), msg))
+	pub fn add_message(&mut self, msg:MessageData) -> Op<MessageData>{
+	    //let msgs: Vec<&MessageData> = self.msgs.read().collect();
+	    for other_msg_id in self.msgs.iter(){
+	        if other_msg_id.value().get_time_stamp() > msg.get_time_stamp(){
+	            let op = self.msgs.insert_after(Some(other_msg_id), msg);
+	            self.msgs.apply(op.clone());
+                return op;
+	        }
+	    }
+	    let op = self.msgs.insert_after(self.msgs.last(), msg);
+        self.msgs.apply(op.clone());
+        return op;
+	}
+	pub fn delete_store(&self) -> std::io::Result<()>{
+	    fs::remove_file(&self.get_filename())?;
+	    Ok(())
 	}
 }
 
@@ -65,8 +116,7 @@ pub struct MessageData {
     pub tag:String,
     pub encrypted_content:String,
     pub signature:String,
-    pub signed_time_stamp:String,
-    pub content:Option<String>
+    pub signed_time_stamp:String
 }
 
 impl MessageData {
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,37 @@
-#[test]
-fn testStorage() {
-    let result = 2 + 2;
-    assert_eq!(result, 4);
+#![allow(unused)]
+use proptest::prelude::*;
+use crate::*;
+
+proptest! {
+    #[test]
+    fn can_create(user in "\\PC*", group in "\\PC*", pass in "\\PC*") {
+        let log = LogState::new(&user, &group, &pass);
+
+        log.save(&pass);
+        assert!(is_store_existing(&log.get_filename()));
+        log.delete_store();
+    }
+
+    #[test]
+    fn can_delete(user in "\\PC*", group in "\\PC*", pass in "\\PC*") {
+        let log = LogState::new(&user, &group, &pass);
+
+        log.save(&pass);
+        log.delete_store();
+        assert!(!is_store_existing(&log.get_filename()));
+    }
+
+    #[test]
+    fn can_read(user in "\\PC*", group in "\\PC*", pass in "\\PC*") {
+        let log_a = LogState::new(&user, &group, &pass);
+
+        log_a.save(&pass);
+
+        let log_b = LogState::new(&user, &group, &pass);
+
+        assert!(log_a == log_b);
+
+        log_a.delete_store();
+    }
 }
+
