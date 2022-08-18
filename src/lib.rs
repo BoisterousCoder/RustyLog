b08b638f0b974d4b79e3622a7deff73e73427d5f
Savannah Jackson <boisterouscoder@gmail.com>
2022-08-17 19:17:28 -0500

    tests are failing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use crdts::glist::{GList, Op};
-use crdts::identifier::Identifier;
 use crdts::CmRDT;
 
 use magic_crypt::MagicCryptTrait;
@@ -12,14 +11,13 @@ use std::fs;
 use std::fs::File;
 use std::error::Error;
 use std::cmp::Ordering;
-use std::iter::Iterator;
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
 use std::io::prelude::*;
 
 
-const SAVE_DIR:&str = ".";
-const FILE_EXT:&str = ".rstore";
+const SAVE_DIR:&str = "./saves";
+const FILE_EXT:&str = "rstore";
 const DATE_FMT:&str = "%Y %b %d %H:%M:%S%.3f %z";
 
 fn fmt_file_name(name:&str, group:&str) -> String{
@@ -48,7 +46,8 @@ pub fn is_store_existing(filename:&str) -> bool{
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
 pub struct LogState {
-    pub msgs:GList<MessageData>,
+    msgs:GList<MessageData>,
+    decrypted_data:Vec<DecryptedData>,
 	pub group:String,
 	pub name:String,
 	pub id_keys: KeyPair
@@ -64,6 +63,7 @@ impl LogState {
         }
         LogState{
             msgs: GList::<MessageData>::new(),
+            decrypted_data: Vec::new(),
             group:group.to_string(),
             name:name.to_string(),
             id_keys: KeyPair{
@@ -72,11 +72,11 @@ impl LogState {
             }
         }
     }
-    pub fn get_filename(&self) -> String {
+    pub fn filename(&self) -> String {
 	 	fmt_file_name(&self.name, &self.group)
 	}
 	pub fn save(&self, password:&str) -> Result<(), Box<dyn Error>>{
-        let mut file = File::create(self.get_filename())?;
+        let mut file = File::create(self.filename())?;
      	let key = new_magic_crypt!(password, 256);
      	let data = key.encrypt_str_to_base64(serde_json::to_string(self)?);
 
@@ -84,42 +84,64 @@ impl LogState {
      	file.sync_all()?;
         Ok(())
 	}
-
-	pub fn add_message(&mut self, msg:MessageData) -> Op<MessageData>{
+	pub fn msgs(&self) -> Vec<&MessageData> {
+	    return self.msgs.iter().map(|msg_id| msg_id.value()).collect();
+	}
+	pub fn decrypt(&mut self, msg:&MessageData, content:&str){
+	    self.decrypted_data.push(DecryptedData{
+            id:msg.get_id(),
+            content:content.to_string()
+	    });
+	}
+	pub fn decrypted(&self, msg:&MessageData) -> Option<String>{
+	    let msg_id = msg.get_id();
+	    for decrypted_data in &self.decrypted_data {
+	        if decrypted_data.id == msg_id {
+	            return Some(decrypted_data.content.clone());
+	        }
+	    }
+	    None
+	}
+	pub fn add_message(&mut self, msg:MessageData) -> String{
 	    //let msgs: Vec<&MessageData> = self.msgs.read().collect();
 	    for other_msg_id in self.msgs.iter(){
 	        if other_msg_id.value().get_time_stamp() > msg.get_time_stamp(){
 	            let op = self.msgs.insert_after(Some(other_msg_id), msg);
 	            self.msgs.apply(op.clone());
-                return op;
+                return serde_json::to_string(&op).unwrap();
 	        }
 	    }
 	    let op = self.msgs.insert_after(self.msgs.last(), msg);
         self.msgs.apply(op.clone());
-        return op;
+        return serde_json::to_string(&op).unwrap();
 	}
-	pub fn delete_store(&self) -> std::io::Result<()>{
-	    fs::remove_file(&self.get_filename())?;
+
+	pub fn apply_op(&mut self, op_data:&str) -> Result<(), Box<dyn Error>>{
+	    let op:Op<MessageData> = serde_json::from_str(op_data)?;
+	    self.msgs.apply(op);
 	    Ok(())
 	}
-}
-
-#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct KeyPair {
-    pub public:String,
-	pub private:String
+	pub fn delete_store(&self) -> std::io::Result<()>{
+	    fs::remove_file(&self.filename())?;
+	    Ok(())
+	}
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MessageData {
     pub from:String,
     pub tag:String,
-    pub encrypted_content:String,
+    pub content:String,
     pub signature:String,
-    pub signed_time_stamp:String
+    pub signed_time_stamp:String//This is not actually signed until after I impliment the rsa stuff
 }
 
 impl MessageData {
+    fn get_id(&self) -> u64{
+        let mut hasher = DefaultHasher::new();
+        format!("{}{}", self.from, self.signed_time_stamp).hash(&mut hasher);
+        return hasher.finish()
+    }
     fn get_time_stamp(&self) -> DateTime<FixedOffset>{
         //TODO: Remove Server Signature here
         match DateTime::parse_from_str(&self.signed_time_stamp, DATE_FMT) {
@@ -155,6 +177,18 @@ impl Ord for MessageData {
             self.from.cmp(&other.from)
         }else{ time_order }
     }
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct DecryptedData {
+    pub id:u64,
+    pub content:String
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct KeyPair {
+    pub public:String,
+	pub private:String
 }
 
 #[cfg(test)]
--- src/tests.rs
+++ src/tests.rs
@@ -2,35 +2,97 @@
 use proptest::prelude::*;
 use crate::*;
 
+//const ALLOWED_CHARS:&str = "[a-zA-Z]+0123456789!@#\\$%\\^&\\*\\(\\)_\\+-=\\{\\}\\[\\]:\";\'<>\\?,\\./\\\\ ";
+const ALLOWED_CHARS:&str = ".";
+
 proptest! {
     #[test]
-    fn can_create(user in "\\PC*", group in "\\PC*", pass in "\\PC*") {
+    fn can_create_store(user in ALLOWED_CHARS, group in ALLOWED_CHARS, pass in ALLOWED_CHARS) {
         let log = LogState::new(&user, &group, &pass);
 
         log.save(&pass);
-        assert!(is_store_existing(&log.get_filename()));
+        assert!(is_store_existing(&log.filename()));
         log.delete_store();
     }
 
     #[test]
-    fn can_delete(user in "\\PC*", group in "\\PC*", pass in "\\PC*") {
+    fn can_delete_store(user in ALLOWED_CHARS, group in ALLOWED_CHARS, pass in ALLOWED_CHARS) {
         let log = LogState::new(&user, &group, &pass);
 
         log.save(&pass);
+        assert!(!is_store_existing(&log.filename()));
         log.delete_store();
-        assert!(!is_store_existing(&log.get_filename()));
     }
 
     #[test]
-    fn can_read(user in "\\PC*", group in "\\PC*", pass in "\\PC*") {
-        let log_a = LogState::new(&user, &group, &pass);
+    fn can_read_store(user in ALLOWED_CHARS, group in ALLOWED_CHARS, pass in ALLOWED_CHARS) {
+        let msg = MessageData {
+            from:"Alice".to_string(),
+            tag:"/s".to_string(),
+            content:"Some base64 data".to_string(),
+            signature:"Some base64 data".to_string(),
+            signed_time_stamp:"1999 Jun 8 12:09:14.274 +0000".to_string()
+        };
+
+        let mut log_a = LogState::new(&user, &group, &pass);
+
+        log_a.add_message(msg);
 
         log_a.save(&pass);
 
         let log_b = LogState::new(&user, &group, &pass);
+        let log_c = LogState::new(&format!("Not {}", user), &group, &pass);
+
 
         assert!(log_a == log_b);
+        assert!(log_a != log_c);
+        log_a.delete_store();
+    }
 
+    #[test]
+    fn can_read_msg(from in ALLOWED_CHARS, tag in ALLOWED_CHARS, content in ALLOWED_CHARS, signature in ALLOWED_CHARS) {
+        let msg = MessageData {
+            from:from,
+            tag:tag,
+            content:content,
+            signature:signature,
+            signed_time_stamp:"1999 Jun 8 12:09:14.274 +0000".to_string()
+        };
+        let pass = "password";
+
+        let mut log_a = LogState::new("Alice", "test", pass);
+
+        log_a.add_message(msg.clone());
+
+        log_a.save(pass);
+
+        let log_b = LogState::new("Alice", "test", pass);
+
+        assert!(log_b.msgs()[0] == &msg);
+        log_a.delete_store();
+    }
+
+    #[test]
+    fn can_read_decrypt_data(from in ALLOWED_CHARS, content in ALLOWED_CHARS) {
+        let msg = MessageData {
+            from:from,
+            tag:"/s".to_string(),
+            content:"Some base64 data".to_string(),
+            signature:"Some base64 data".to_string(),
+            signed_time_stamp:"2022 Aug 17 1:09:01.666 +0000".to_string()
+        };
+        let pass = "password";
+
+        let mut log_a = LogState::new("Alice", "test", pass);
+
+        log_a.add_message(msg.clone());
+        log_a.decrypt(&msg, &content);
+
+        log_a.save(pass);
+
+        let log_b = LogState::new("Alice", "test", pass);
+
+        assert!(log_b.decrypted(&msg) == Some(content));
         log_a.delete_store();
     }
 }
